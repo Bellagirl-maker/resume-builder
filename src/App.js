ea2f7dd16e5719235b4613fc70904bd50dab6c81
Isabella <isabellaotoo25@gmail.com>
2023-11-28 08:33:24 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,25 @@
 import './App.css';
+import  { useState } from "react";
 
 function App() {
-  
-  const users = [
-    { name: "Isabella", age:21 },
-    { name: "Freda", age:22 },
-    { name: "Nathan", age:23 },
-    ];
-return (
+
+  const [age, setAge] = useState(0);
+  const increaseAge = () => {
+    setAge(age + 1);
+  }
+  return (
     <div className='App'>
-      {users.map((user, key) =>{
-       return(
-        <Users name={user.name} age={user.age} />
-       )
-
-      })}
-
-      {/* {age >= 18 ? <h1>OVERAGE</h1>:<h1>UNDERAGE</h1>}
-      <h1 style={{color: isRed ? "red" : "green"}}> THIS IS COLOR</h1>
-      {isRed && <button>THIS IS A BUTTON</button>} */}
-     </div>
-    // <div>
-    // <User name="Isabella" age={21} email="isabella@bella.com" />
-    // <User name="Freda" age={34} email="freda@bella.com" />
-    // <User name="Neeta" age={18} email="Neeta@bella.com" />
-    // <Job salary="9000" position="SeniorSDE" company="Amazon" />
-    // </div>
+      {age}
+      <button onClick={increaseAge}>Increase Age</button> 
+    </div>
   );
+  
+  
 }
 
-const Users = (props) => {
-  return <h1>{props.name} {props.age}</h1>
-}
+// const Users = (props) => {
+//   return <h1>{props.name} {props.age}</h1>
+// }
 
 // const User = (props) =>{
 //   return (

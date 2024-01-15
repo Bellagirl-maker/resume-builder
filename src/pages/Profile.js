0ceb2a5282884c00dd344f3e87d2f78caf59246c
Isabella <isabellaotoo25@gmail.com>
2024-01-15 11:25:52 +0000

    Update Profile with UseNavigate

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,13 @@
+import { useNavigate } from "react-router-dom"
+
 export const Profile = () => {
+    let navigate = useNavigate();
     return (
         <div>
             <h1>THIS IS THE PROFILE PAGE</h1>
+            <button onClick={() => {
+                navigate("/contact");
+            }}>GO TO CONTACT PAGE</button>
         </div>
     )
 

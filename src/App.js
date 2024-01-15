219fbb53ce863f85a57f943017fff10012c87533
Isabella <isabellaotoo25@gmail.com>
2024-01-15 10:50:45 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar } from './Navbar'
 import { Home } from './pages/Home';
+import { Profile } from './pages/Profile';
+import { Contact } from './pages/Contact';
 // import  { useState } from "react";
 // import Axios from "axios";
 
@@ -8,8 +11,11 @@ function App() {
   return (
     <div className='App'>
       <Router>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/' element ={<Home />} />
+          <Route path='/profile' element ={<Profile />} />
+          <Route path='/contact' element ={<Contact />} />
         </Routes>
       </Router>
     </div>
@@ -37,6 +43,7 @@ function App() {
   //   </div>
   // );
 }
+export default App;
 
 // function App() {
 //   const[catFact, setCatFact] = useState("");
@@ -98,4 +105,4 @@ function App() {
 //   )
 // }
 
-export default App;
+

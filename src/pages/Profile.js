c7d1840a2a5086272c7ba270fb8f9e274d135f89
Isabella <isabellaotoo25@gmail.com>
2024-01-16 20:17:21 +0000

    Update profile.js

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,11 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 
 export const Profile = () => {
     let navigate = useNavigate();
+    let { username } = useParams;
     return (
         <div>
-            <h1>THIS IS THE PROFILE PAGE</h1>
+            <h1>THIS IS THE PROFILE PAGE for {username}!</h1>
             <button onClick={() => {
                 navigate("/contact");
             }}>GO TO CONTACT PAGE</button>

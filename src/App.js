b6e31af870cc6680368168a9b552b54593fed3a3
Isabella <isabellaotoo25@gmail.com>
2024-01-16 23:46:43 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element ={<Home />} />
-          <Route path='/profile' element ={<Profile />} />
+          <Route path='/profile/:username' element ={<Profile />} />
           <Route path='/contact' element ={<Contact />} />
         </Routes>
       </Router>
